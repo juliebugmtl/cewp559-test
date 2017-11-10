93ae999c227ce15dc105139a4d38d44fd8437a5c
Julie Martin <subs+github@juliemartin.org>
2017-11-09 20:36:13 -0500

    Added Stripe

---
+++ source/client/stripe.js
@@ -0,0 +1,26 @@
+var handler = StripeCheckout.configure({
+  key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+  locale: 'auto',
+  token: function(token) {
+    console.log(token);
+    // You can access the token ID with `token.id`.
+    // Get the token ID to your server-side code for use.
+  }
+});
+
+document.getElementById('customButton').addEventListener('click', function(e) {
+  // Open Checkout with further options:
+  handler.open({
+    name: 'Julie\'s Shop',
+    description: 'Hot dog lunch',
+    zipCode: true,
+    amount: 999
+  });
+  e.preventDefault();
+});
+
+// Close Checkout on page navigation:
+window.addEventListener('popstate', function() {
+  handler.close();
+});
